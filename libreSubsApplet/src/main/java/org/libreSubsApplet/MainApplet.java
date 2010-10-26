a682e7a65260c61b51bcc0321c14f0f2bcbd0c49
lcsantos <lucass@gmail.com>
2010-10-26 14:00:07 -0200

    Added applet module with test drag and drop

---
+++ libreSubsApplet/src/main/java/org/libreSubsApplet/MainApplet.java
@@ -0,0 +1,16 @@
+package org.libreSubsApplet;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JApplet;
+import javax.swing.JLabel;
+
+@SuppressWarnings("serial")
+public class MainApplet extends JApplet{
+
+	@Override
+	public void init() {
+      setLayout(new BorderLayout());
+      add(new JLabel("fdebhfjhb"),BorderLayout.CENTER);
+	}
+}
