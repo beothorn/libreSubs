ba9780e6e50f744781aad8e7a58e5d9b706456c2
lucas <lucass@gmail.com>
2011-01-15 04:24:46 -0200

    made menu inline

--- libreSubsWicketSite/src/main/java/libreSubs/libreSubsSite/BasePage.java
+++ libreSubsWicketSite/src/main/java/libreSubs/libreSubsSite/BasePage.java
@@ -5,14 +5,18 @@ import libreSubs.libreSubsSite.editPage.EditSubtitleFormPage;
 import libreSubs.libreSubsSite.recentChanges.RecentChangesPage;
 import libreSubs.libreSubsSite.upload.UploadFormPage;
 
+import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.CSSPackageResource;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
 public abstract class BasePage extends WebPage {
 	
 	public BasePage() {
 		add(CSSPackageResource.getHeaderContribution(BasePage.class,"style.css")); 
+		add(new Image("headerIcon", new ResourceReference(BasePage.class, "headerIcon.png")));
+		add(new Image("libreSubsLogo", new ResourceReference(BasePage.class, "libreSubsLogo.png")));
 		add(new BookmarkablePageLink<String>("home", HomePage.class));
 		add(new BookmarkablePageLink<String>("downloadSub",DownloadFormPage.class));
 		add(new BookmarkablePageLink<String>("uploadSub", UploadFormPage.class));
