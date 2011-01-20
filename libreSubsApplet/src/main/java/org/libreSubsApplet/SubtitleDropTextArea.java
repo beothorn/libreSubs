5d6557f586b7b3280001659ea698027ec667b588
lucas <lucass@gmail.com>
2011-01-19 22:43:35 -0200

    Added empty border to applet

--- libreSubsApplet/src/main/java/org/libreSubsApplet/SubtitleDropTextArea.java
+++ libreSubsApplet/src/main/java/org/libreSubsApplet/SubtitleDropTextArea.java
@@ -2,7 +2,6 @@ package org.libreSubsApplet;
 
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Insets;
 import java.awt.Rectangle;
 import java.net.URL;
 
@@ -19,8 +18,7 @@ public class SubtitleDropTextArea extends JTextArea {
 		setOpaque(false);
 		setBorder(null);
 		setEditable(false);
-		setMargin(new Insets(100,100,100,100)); 
-		
+		setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		final URL resource = SubtitleDropTextArea.class.getResource("/applet.png");
 		final ImageIcon background = new ImageIcon(resource);
 		image = background.getImage();
